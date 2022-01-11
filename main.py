e7246c307bdba7903350577dcdce9f80346d1e88
eunsu28 <ayay.e2008@gmail.com>
2022-01-11 17:36:29 +0900

    간격을 두고 프린팅 하는것이 작동되지 않음

--- main.py
+++ main.py
@@ -5,4 +5,13 @@ first_number = random.randrange(1, 8)
 second_number = random.randrange(1, 8)
 third_number = random.randrange(1, 8)
 
-print("")
+print("YOUR NUMBER: ", end="")
+sleep(1000)
+print(first_number, end="")
+sleep(1000)
+print(second_number, end="")
+sleep(1000)
+print(third_number)
+sleep(1000)
+
+
