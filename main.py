a33a2a4e89b7fd2afb29bd4e66351200d17321ea
eunsu28 <ayay.e2008@gmail.com>
2022-01-11 16:02:11 +0900

    import time to use time.sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
+from time import sleep
 
 first_number = random.randrange(1, 8)
 second_number = random.randrange(1, 8)
 third_number = random.randrange(1, 8)
-print(first_number, second_number, third_number)
