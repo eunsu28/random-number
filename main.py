472fcd7e26918bc8e6115f82c3535049d54956cb
eunsu28 <ayay.e2008@gmail.com>
2022-01-11 17:44:33 +0900

    sleep was the problem

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ second_number = random.randrange(1, 8)
 third_number = random.randrange(1, 8)
 
 print("YOUR NUMBER: ", end="")
-sleep(10)
+#sleep(10)
 print(first_number, end="")
-sleep(10)
+#sleep(10)
 print(second_number, end="")
-sleep(10)
+#sleep(10)
 print(third_number)
-sleep(10)
+#sleep(10)
 
 
