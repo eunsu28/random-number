009b0fcfeb1daf3dbb95f43e2e602f3fec562ac0
eunsu28 <ayay.e2008@gmail.com>
2022-08-02 09:45:03 +0900

    really finished

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
+#import things
 import random
 import time
+#
 
+#generate numbers
 first_number = random.randrange(1, 3)
 second_number = random.randrange(1, 3)
 third_number = random.randrange(1, 3)
+#
 
+#print it
 print("YOUR NUMBER: ")
 time.sleep(2)
 print(first_number)
@@ -12,12 +17,16 @@ time.sleep(2)
 print(second_number)
 time.sleep(2)
 print(third_number)
+#
 
+#show the all numbers at last
 time.sleep(1)
 print("YOUR NUMBER is ", end="")
 print(first_number, end="")
 print(second_number, end="")
 print(third_number)
-#finish
+#
+
+#finish  끝
 
 
