1aaac966bae567d53751f048286ddbfa191d176f
eunsu28 <ayay.e2008@gmail.com>
2022-08-02 09:41:15 +0900

    successful in other way

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ first_number = random.randrange(1, 3)
 second_number = random.randrange(1, 3)
 third_number = random.randrange(1, 3)
 
-print("YOUR NUMBER: ", end="")
-print(first_number, end="")
-time.sleep(1)
-print(second_number, end="")
-time.sleep(1)
+print("YOUR NUMBER: ")
+time.sleep(2)
+print(first_number)
+time.sleep(2)
+print(second_number)
+time.sleep(2)
 print(third_number)
 #finish
 
